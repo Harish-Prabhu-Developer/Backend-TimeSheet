4ff9470d5b0a04a9384ea599cfbf19671e144d2a
HARISH PRABHU <112898522+Harish-Prabhu-Developer@users.noreply.github.com>
2025-01-02 00:12:13 +0530

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   service: 'gmail', 
   auth: {
-    user: 'harishpraharshu@gmail.com',
-    pass: 'jmfi qrix ptho kmql'
+    user: '',
+    pass: ''
   }
 });
 
